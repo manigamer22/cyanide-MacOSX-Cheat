1797436873cf434691f73229985dc342ffab9091
Manigamer22 <32180091+manigamer22@users.noreply.github.com>
2020-06-05 21:17:36 +0800

    New csgo Update fix

--- Cyanide-master/Cyanide/Hooks.cpp
+++ Cyanide-master/Cyanide/Hooks.cpp
@@ -6,17 +6,17 @@ void InitializeInterfaces() {
     pPanel = GetInterface<IPanel>("./bin/osx64/vgui2.dylib", "VGUI_Panel");
     pCEffects = GetInterface<CEffects>("./bin/osx64/engine.dylib", "VEngineEffects");
     pCvar = GetInterface<ICvar>("./bin/osx64/materialsystem.dylib", "VEngineCvar");
-    pClient = GetInterface<IBaseClientDLL>("./csgo/bin/osx64/client_panorama.dylib", "VClient");
+    pClient = GetInterface<IBaseClientDLL>("./csgo/bin/osx64/client.dylib", "VClient");
     pEngine = GetInterface<IEngineClient>("./bin/osx64/engine.dylib", "VEngineClient");
-    pEntList = GetInterface<IClientEntityList>("./csgo/bin/osx64/client_panorama.dylib", "VClientEntityList");
+    pEntList = GetInterface<IClientEntityList>("./csgo/bin/osx64/client.dylib", "VClientEntityList");
     pOverlay = GetInterface<IVDebugOverlay>("./bin/osx64/engine.dylib", "VDebugOverlay");
     pEngineTrace = GetInterface<IEngineTrace>("./bin/osx64/engine.dylib", "EngineTraceClient");
     pModelInfo = GetInterface<IVModelInfo>("./bin/osx64/engine.dylib", "VModelInfoClient");
     pInputSystem = GetInterface<IInputSystem>("./bin/osx64/inputsystem.dylib", "InputSystemVersion");
     pModelRender = GetInterface<IVModelRender>("./bin/osx64/engine.dylib", "VEngineModel");
     pMatSystem = GetInterface<IMaterialSystem>("./bin/osx64/materialsystem.dylib", "VMaterialSystem");
-    pPrediction = GetInterface<IPrediction>("./csgo/bin/osx64/client_panorama.dylib", "VClientPrediction");
-    pGameMovement = GetInterface<IGameMovement>("./csgo/bin/osx64/client_panorama.dylib", "GameMovement");
+    pPrediction = GetInterface<IPrediction>("./csgo/bin/osx64/client.dylib", "VClientPrediction");
+    pGameMovement = GetInterface<IGameMovement>("./csgo/bin/osx64/client.dylib", "GameMovement");
     pPhysics = GetInterface<IPhysicsSurfaceProps>("./bin/osx64/vphysics.dylib", "VPhysicsSurfaceProps");
     pEngineVGui = GetInterface<IEngineVGui>("./bin/osx64/engine.dylib", "VEngineVGui");
     pGameEventManager = GetInterface<IGameEventManager2>("./bin/osx64/engine.dylib", "GAMEEVENTSMANAGER002", true);
@@ -32,9 +32,9 @@ void ProtectAddr(void* addr, int prot)
 void InitializeVMTs() {
     painthook = new VMT(pPanel);
     
-    uintptr_t findClientMode = CPatternScanner::Instance()->GetPointer("client_panorama.dylib", (unsigned char*)"\x48\x8B\xB7\x00\x00\x00\x00\x48\x8D\x3D\x00\x00\x00\x00\x5D\xE9", "xxx????xxx????xx", 0xA) + 0x4;
+    uintptr_t findClientMode = CPatternScanner::Instance()->GetPointer("client.dylib", (unsigned char*)"\x48\x8B\xB7\x00\x00\x00\x00\x48\x8D\x3D\x00\x00\x00\x00\x5D\xE9", "xxx????xxx????xx", 0xA) + 0x4;
     
-    uintptr_t findGlobalVars = CPatternScanner::Instance()->GetPointer("client_panorama.dylib", (unsigned char*)"\x48\x8D\x05\x00\x00\x00\x00\x48\x8B\x00\xF3\x0F\x10\x00\x00\xF3\x0F\x11\x83\x00\x00\x00\x00", "xxx????xxxxxx??xxxx????", 0x3) + 0x4;
+    uintptr_t findGlobalVars = CPatternScanner::Instance()->GetPointer("client.dylib", (unsigned char*)"\x48\x8D\x05\x00\x00\x00\x00\x48\x8B\x00\xF3\x0F\x10\x00\x00\xF3\x0F\x11\x83\x00\x00\x00\x00", "xxx????xxxxxx??xxxx????", 0x3) + 0x4;
 
     uintptr_t booladdr = CPatternScanner::Instance()->GetProcedure("engine.dylib", (unsigned char*)"\x41\xB5\x00\x84\xC0\x74\x11", "xx?xxxx", 0x1) + 0x2;
     //booladdr = GetAbsoluteAddress(booladdr, 2, 1);
@@ -42,15 +42,15 @@ void InitializeVMTs() {
     ProtectAddr(bSendPacket, PROT_READ | PROT_WRITE | PROT_EXEC);
     //vm_protect(mach_task_self(), (vm_address_t)bSendPacket, 1, 0, VM_PROT_ALL);
     
-    uintptr_t findRankReveal = CPatternScanner::Instance()->GetPointer("client_panorama.dylib", (unsigned char*) "\x48\x89\x85\x28\xFE\xFF\xFF\x48\xC7\x85\x30\xFE\xFF\xFF\x00\x00\x00\x00\x48\x8D\x05\x00\x00\x00\x00", "xxxxxxxxxxxxxx????xxx????", 0x15) + 0x4;
+    uintptr_t findRankReveal = CPatternScanner::Instance()->GetPointer("client.dylib", (unsigned char*) "\x48\x89\x85\x28\xFE\xFF\xFF\x48\xC7\x85\x30\xFE\xFF\xFF\x00\x00\x00\x00\x48\x8D\x05\x00\x00\x00\x00", "xxxxxxxxxxxxxx????xxx????", 0x15) + 0x4;
     
     uintptr_t findStartDrawing = CPatternScanner::Instance()->GetPointer("vguimatsurface.dylib", (unsigned char*) "\xFF\x90\x00\x00\x00\x00\x48\x89\xC3\x48\x85\xDB\x74\x00", "xx????xxxxxxx?", 0xD) + 0x4;
     
     uintptr_t findClanTag = CPatternScanner::Instance()->GetPointer("engine.dylib", (unsigned char*) "\x48\x8D\x3D\x00\x00\x00\x00\x48\x89\xFE\xE8\x00\x00\x00\x00\xE9\x00\x00\x00\x00", "xxx????xxxx????x????", 0xB) + 0x4;
     
-    uintptr_t predictionSeedPointer = CPatternScanner::Instance()->GetPointer("client_panorama.dylib", (unsigned char*)"\x48\x8D\x0D\x00\x00\x00\x00\x89\x01\x5D\xC3", "xxx????xxxx", 0x3) + 0x4;
+    uintptr_t predictionSeedPointer = CPatternScanner::Instance()->GetPointer("client.dylib", (unsigned char*)"\x48\x8D\x0D\x00\x00\x00\x00\x89\x01\x5D\xC3", "xxx????xxxx", 0x3) + 0x4;
     
-    void* handle = dlopen("./csgo/bin/osx64/client_panorama.dylib", RTLD_NOLOAD | RTLD_NOW);
+    void* handle = dlopen("./csgo/bin/osx64/client.dylib", RTLD_NOLOAD | RTLD_NOW);
     RandomInt = reinterpret_cast<RandomIntFn>(dlsym(handle, "RandomInt"));
     dlclose(handle);
     
